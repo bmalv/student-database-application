851ca24a2c391fca0f013453bc8b8e469652cb88
bmalv <74278503+bmalv@users.noreply.github.com>
2024-10-15 16:04:54 -0500

    creating class outline

---
+++ src/Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int gradeLevel;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+
+    public Student(){
+
+    }
+
+    //create 5 digit unique ID
+
+    //enroll in courses
+
+    //view tuition balance
+
+    //pay tuition balance
+
+    //print out student status: name, ID, courses enrolled, and balance
+}
---
+++ src/StudentDatabaseApp.java
@@ -0,0 +1,7 @@
+public class StudentDatabaseApp {
+    public static void main(String[] args) {
+        //Ask how many students we want to add
+
+        // create n number of new students
+    }
+}
