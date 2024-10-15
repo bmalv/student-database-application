2e83e467b4bd8d6240fb5c76c60feb7e685c5ed8
bmalv <74278503+bmalv@users.noreply.github.com>
2024-10-15 16:09:59 -0500

    prompting user for student information

--- src/Student.java
+++ src/Student.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Student {
     private String firstName;
     private String lastName;
@@ -8,7 +10,19 @@ public class Student {
     private int costOfCourse = 600;
 
     public Student(){
+        //prompting user for student info
+        Scanner in = new Scanner(System.in);
 
+        System.out.print("Enter student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.print("Enter student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.print("1 - Freshmen\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student grade level: ");
+        this.gradeLevel = in.nextInt();
+
+        System.out.println(firstName + " " + lastName + " " + gradeLevel);
     }
 
     //create 5 digit unique ID
--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -1,7 +1,7 @@
 public class StudentDatabaseApp {
     public static void main(String[] args) {
         //Ask how many students we want to add
-
+        Student student1 = new Student();
         // create n number of new students
     }
 }
