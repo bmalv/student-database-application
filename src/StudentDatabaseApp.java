77a756570428d642988ee0586c641de8dcd20bc1
bmalv <74278503+bmalv@users.noreply.github.com>
2024-10-16 09:55:44 -0500

    creating n students

--- src/StudentDatabaseApp.java
+++ src/StudentDatabaseApp.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class StudentDatabaseApp {
     public static void main(String[] args) {
+
         //Ask how many students we want to add
-        Student student1 = new Student();
-        student1.enroll();
-        student1.payBalance();
-        System.out.print(student1.showInfo());
+        System.out.print("Enter number of students to enroll: ");
+        Scanner in = new Scanner(System.in);
+        int numOfStudents = in.nextInt();
+        Student[] students = new Student[numOfStudents];
+
         // create n number of new students
+        for(int n = 0; n < numOfStudents; n++){
+            students[n] = new Student();
+            students[n].enroll();
+            students[n].payBalance();
+        }
+
+        for(int n = 0; n < numOfStudents; n++){
+            System.out.println(students[n].showInfo());
+        }
     }
 }
